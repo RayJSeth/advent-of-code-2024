53466e9dc889468bbd697ee802081f4591bef52d
Seth <seth@rayjseth.io>
2024-12-03 10:17:19 -0800

    day2part2

--- days/two.go
+++ days/two.go
@@ -11,47 +11,68 @@ import (
 	"rayjseth.io/aoc-24/model"
 )
 
-type reports [][]int
+type Report []int
+type Reports []Report
 
 func Two() model.Result {
 	day := uint8(2)
 	reports := parseDay2Input("./inputs/day2")
-	return model.Result{Day: &day, Part1: reports.calcPart1()}
+	return model.Result{Day: &day, Part1: reports.calcPart1(), Part2: reports.calcPart2()}
 }
 
-func (r reports) calcPart1() *int {
+func (r Reports) calcPart1() *int {
 	nSafe := 0
-
-reportsLoop:
 	for i, report := range r {
 		if len(report) < 2 {
 			log.Fatalf("Error line %d - each line must have at least two numbers", i)
 		}
-
-		iDiff := report[1] - report[0]
-		if isSevereDelta(iDiff) {
-			continue
+		if report.isSafe() {
+			nSafe++
 		}
-		isAsc := iDiff > 0
-
-		// start at 2 since this is a lookback and first pair already processed above
-		for j := 2; j < len(report); j++ {
-			diff := report[j] - report[j-1]
-			if isSevereDelta(diff) || isAsc != (diff > 0) {
-				continue reportsLoop
-			}
-		}
-		nSafe++
 	}
 
 	return &nSafe
 }
 
-func isSevereDelta(diff int) bool {
+func (r Reports) calcPart2() *int {
+	nSafe := 0
+
+reportsLoop:
+	for i, report := range r {
+		if len(report) < 3 {
+			log.Fatalf("Error line %d - each line must have at least three numbers", i)
+		}
+
+		for j := 0; j < len(report); j++ {
+			modifiedReport := append(append(Report{}, report[:j]...), report[j+1:]...)
+			// TODO, maybe return unsafe idx and target modifications rather than brute force
+			// challenge would be discerning if violation is curr or lookbehind to know where
+			// to cut
+			if modifiedReport.isSafe() {
+				nSafe++
+				continue reportsLoop
+			}
+		}
+	}
+	return &nSafe
+}
+
+func (r Report) isSafe() bool {
+	isAsc := r[1] > r[0]
+	for i := 1; i < len(r); i++ {
+		diff := r[i] - r[i-1]
+		if isInvalidDelta(diff) || isAsc != (diff > 0) {
+			return false
+		}
+	}
+	return true
+}
+
+func isInvalidDelta(diff int) bool {
 	return diff == 0 || math.Abs((float64(diff))) > 3
 }
 
-func parseDay2Input(filePath string) reports {
+func parseDay2Input(filePath string) Reports {
 	file, err := os.Open(filePath)
 	if err != nil {
 		log.Fatalf("Error opening file at %s", filePath)
@@ -59,7 +80,7 @@ func parseDay2Input(filePath string) reports {
 	}
 	defer file.Close()
 
-	var parsed reports
+	var parsed Reports
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		var levels []int
